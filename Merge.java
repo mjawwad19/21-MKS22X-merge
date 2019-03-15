36fc07f8ddb7f945eae3606773b3ed1542c474ca
Mohammad Jawwad <mjawwad@stuy.edu>
2019-03-15 14:51:06 -0400

    added main [1] test case

--- Merge.java
+++ Merge.java
@@ -1,14 +1,17 @@
+import java.util.*;
 public class Merge{
+  /*sort the array from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[]data) {
     mergesort(data, 0, data.length -1);
   }
   private static void mergesort(int[]data, int lo, int hi) {
     if (lo >= hi) return ;
+    System.out.println(Arrays.toString(data));
     mergesort(data, lo, midP(data));
     mergesort(data, midP(data) + 1, hi);
     //mergeUP somehow
   }
-
+  //finds the middle to partition mergesort left and right
   private static int midP(int[]data) {
     if (data.length % 2 == 1) {
       return data.length/2 + 1;
@@ -16,11 +19,12 @@ public class Merge{
     else return data.length/2;
   }
 
-  private static void mergeUP (int[] l, int[] r, int[] above) {
-    for (int i = 0; i < l.length; i++) {
-      if (l[i] <= r[i]) above[i] = l[i];
-      else above[i] = r[i];
-    }
+  public static void main(String[] args) {
+    int[] ary = new int[]{38, 27, 43, 3, 9, 83, 10};
+    System.out.println("original : " + Arrays.toString(ary));
+    mergesort(ary);
+    System.out.println("sorted : " + Arrays.toString(ary));
+    //[3,9,10,27,38,43,83];
   }
 
 }
